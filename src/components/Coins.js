8bb0377f0e8531528dded0ace803f5f74c248933
Josh <ahimaz.joshua@gmail.com>
2020-11-04 15:21:33 +0800

    Changed to custom table instead

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -4,73 +4,62 @@ import BootstrapTable from 'react-bootstrap-table-next';
 
 function Posts({coins, loading}){
 
-    const columns = [{
-        dataField: 'market_cap_rank',
-        text: '#',
-        sort: true
-    }, {
-        dataField: 'name',
-        text: 'Coin',
-        sort: true
-    }, {
-        dataField: 'symbol',
-        text: '',
-        sort: false
-    }, {
-        dataField: 'current_price',
-        text: 'Price',
-        sort: true
-    }, {
-        dataField: 'total_volume',
-        text: '24h Volume',
-        sort: true
-    }, {
-        dataField: `dsada`,
-        text: 'Last 7 Days',
-        sort: false
-    }];
-
-    const defaultSorted = [{
-    dataField: 'market_cap_rank',
-    order: 'asc'
-    }];
-
     if(loading){
-        return <h2>Loading coins...</h2>
+        return(
+            <div className="reactive-table">
+            <table>
+                <tr>
+                    <th class="number">#</th>
+                    <th class="coin-image"></th>
+                    <th class="coin">Coin</th>
+                    <th class="symbol"> </th>
+                    <th class="price">Price</th>
+                    <th class="Volume">24h Volume</th>
+                    <th class="Sparkline">Last 7 Days</th>
+                </tr>
+                <tbody>
+                    <tr>
+                        <td>#</td>
+                        <td></td>
+                        <td>Loading...</td>
+                        <td></td>
+                        <td>Loading...</td>
+                        <td>Loading...</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        );
     }
     
     return (
-        <BootstrapTable
-            className="react-bootstrap-table"
-            keyField="id"
-            data={ coins }
-            columns={ columns }
-            defaultSorted={ defaultSorted } 
-            />
-        // <BootstrapTable keyField='id' data={ coins } columns={ columns } bordered={ false } />
         
-    // <div>
-    //     <table>
-    //         <tr>
-    //             <th>#</th>
-    //             <th>Coin</th>
-    //             <th> </th>
-    //             <th>Price</th>
-    //             <th>24h Volume</th>
-    //         </tr>
-    //         <tbody>
-    //         {coins.map(coin => (
-    //             <tr>
-    //                 <td>5</td>
-    //                 <td>{coin.name}</td>
-    //                 <td>{coin.symbol}</td>
-    //                 <td>{coin.current_price}</td>
-    //                 <td>{coin.total_volume}</td>
-    //             </tr>
-    //         ))}
-    //         </tbody>
-    //     </table>
-    // </div>
+    <div className="reactive-table">
+        <table>
+            <tr>
+                <th class="number">#</th>
+                <th class="coin-image"></th>
+                <th class="coin">Coin</th>
+                <th class="symbol"> </th>
+                <th class="price">Price</th>
+                <th class="Volume">24h Volume</th>
+                <th class="Sparkline">Last 7 Days</th>
+            </tr>
+            <tbody>
+            {coins.map(coin => (
+                <tr>
+                    <td>{coin.market_cap_rank}</td>
+                    <td><img src={coin.image} className="coin-icon"/></td>
+                    <td>{coin.name}</td>
+                    <td>{coin.symbol}</td>
+                    <td>{coin.current_price}</td>
+                    <td>{coin.total_volume}</td>
+                    <td>Sparkline Goes Here</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    </div>
 
     // <ul className="coins-list">
     //     {coins.map(coin => (
